7b7cd2c75b4497f3004d64d6b84a3804a4661ba9
Sharath Vignesh <sharathvignesh08@gmail.com>
2017-01-07 21:27:27 +0900

    Production env

--- webpack.config.js
+++ webpack.config.js
@@ -5,11 +5,7 @@ var BUILD_DIR = path.resolve(__dirname, 'public/js');
 var APP_DIR = path.resolve(__dirname, 'app');
 
 var config = {
-  devtool: 'inline-source-map',
-  entry: [
-    'webpack-dev-server/client?http://127.0.0.1:3000/',
-    APP_DIR + '/main.js'
-  ],
+  entry: APP_DIR + '/main.js',
   output: {
     path: path.join(__dirname, 'dist'),
     publicPath: "http://localhost:3000/dist/",
@@ -33,8 +29,11 @@ var config = {
     ]
   },
   plugins: [
-      new webpack.HotModuleReplacementPlugin(),
-      new webpack.NoErrorsPlugin()
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify('production')
+      }
+    })
   ]
 };
 
